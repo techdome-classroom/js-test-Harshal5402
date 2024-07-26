343ee32a53309eab753d738121de8eb130e8a9e3
Harshal Ingle <harshalingle11092001@gmail.com>
2024-07-26 13:31:08 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,6 +1,7 @@
 const getTotalIsles = function (grid) {
-  if (!grid || grid.length === 0) return 0;
-    
+  if (!grid || grid.length === 0) {
+    return 0;
+  }
     const rows = grid.length;
     const cols = grid[0].length;
     const visited = Array.from({ length: rows }, () => Array(cols).fill(false));
