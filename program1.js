d188dfda28dcc2b2be978e82449c7098e9c67ca9
Harshal Ingle <harshalingle11092001@gmail.com>
2024-07-26 13:37:30 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -21,7 +21,7 @@ const getTotalIsles = function (grid) {
     
     for (let i = 0; i < row; i++) {
         for (let j = 0; j < cols; j++) {
-            if (condition) {
+            if (grid[i][j] === "L") {
                 
             }
             
