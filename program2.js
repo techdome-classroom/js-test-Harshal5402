34c32898f7d5e1f73df26553ed3ae910aad476aa
Harshal Ingle <harshalingle11092001@gmail.com>
2024-07-26 13:41:54 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -2,7 +2,7 @@ const decodeTheRing = function (s, p) {
   let sindex = 0;
   let pindex = 0;
 
-  while(sindex < s.length && pindex) < 
+  while(sindex < s.length && pindex )
   };
   
   module.exports = decodeTheRing;
